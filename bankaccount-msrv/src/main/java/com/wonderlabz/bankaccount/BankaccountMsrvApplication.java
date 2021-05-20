3ac2ccffaef2e8b8946961656cd8c8d41b95fb44
codeManga1 <38323070+codeManga1@users.noreply.github.com>
2021-05-20 22:00:17 +0200

    Add files via upload

---
+++ bankaccount-msrv/src/main/java/com/wonderlabz/bankaccount/BankaccountMsrvApplication.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaccountMsrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankaccountMsrvApplication.class, args);
+    }
+
+}
---
+++ bankaccount-msrv/src/test/java/com/wonderlabz/bankaccount/BankaccountMsrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaccountMsrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
