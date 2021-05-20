31bbcf9a9b9b5215498fa269665c8e53c36baf80
hilton9 <hiltonm@discovery.co.za>
2021-05-20 22:49:38 +0200

    adding swagger ui to test end-point of the api

--- target/generated-sources/annotations/com/wonderlabz/bankaccount/mappers/UserEntityMapperImpl.java
+++ target/generated-sources/annotations/com/wonderlabz/bankaccount/mappers/UserEntityMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-20T22:39:21+0200",
+    date = "2021-05-20T22:45:31+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.3 (JetBrains s.r.o)"
 )
 public class UserEntityMapperImpl implements UserEntityMapper {
